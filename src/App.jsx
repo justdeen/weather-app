a940b9726f6485c5a9804b644ac5c78b75ec4b12
Sheriff <sheriffaliuyamah82@gmail.com>
2025-10-22 03:17:13 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import Search from './components/Search.jsx'
+import Toggle from './components/Toggle.jsx'
+
+function App() {
+  return (
+    <>
+      <Toggle />
+      <Search />
+    </>
+  )
+}
+
+export default App
