954237689007313b794d600548a9b626f465e415
Sheriff <sheriffaliuyamah82@gmail.com>
2025-10-22 01:27:22 -0700

    Updates

--- src/test.js
+++
@@ -1,9 +0,0 @@
-let callApi = async (api, city) => {
-    let res = await fetch(`http://api.weatherapi.com/v1/current.json?key=${api}&q=${city}`);
-    let res1 = await res.json();
-    console.log(res1.current);
-  };
-
-//   callApi("4558d53f9641485fb2a114816251910", "paris");
-
-    export { callApi };
