00fda1458b42dfa837e1dab4a96f8c6776a371a2
Sheriff <sheriffaliuyamah82@gmail.com>
2025-10-25 02:41:03 -0700

    Edited Geolocation

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,12 +1,12 @@
-import React, { useState, useEffect } from "react";
+import React, {useState, useEffect} from "react";
 import useWeather from "../hooks/useWeather";
 import Today from "./Today";
 import Hourly from "./Hourly";
-import Daily from "./Daily"
+import Daily from "./Daily";
 import Spinner from "./Spinner";
 import ErrorMessage from "./ErrorMessage";
 import UserError from "./UserError";
-import './Search.css'
+import "./Search.css";
 
 const WEATHER_API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
 const MAP_API_KEY = import.meta.env.VITE_MAP_API_KEY;
@@ -16,27 +16,27 @@ export default function App({navElements}) {
   const [city, setCity] = useState("");
   const [userError, setUserError] = useState(null);
   const [userCity, setUserCity] = useState("");
-  const { todayData, hourlyData, dailyData, loading, setLoading, error, fetchByCity } = useWeather(WEATHER_API_KEY, MAP_API_KEY);
+  const {todayData, hourlyData, dailyData, loading, setLoading, error, fetchByCity} = useWeather(WEATHER_API_KEY, MAP_API_KEY);
 
   useEffect(() => {
-    setUserError(null)
+    setUserError(null);
     if (!navigator.geolocation) return;
     navigator.geolocation.getCurrentPosition(success, denied);
     async function success(position) {
-        const lat = position.coords.latitude;
-        const lon = position.coords.longitude;
-        const res = await fetch(`${BASE}?key=${WEATHER_API_KEY}&q=${lat},${lon}`);
-        const data = await res.json();
-        setUserCity(data.location.name);
-        if (userCity) fetchByCity(userCity);
+      const lat = position.coords.latitude;
+      const lon = position.coords.longitude;
+      const res = await fetch(`${BASE}?key=${WEATHER_API_KEY}&q=${lat},${lon}`);
+      const data = await res.json();
+      setUserCity(data.location.name);
+      if (userCity) fetchByCity(userCity);
     }
     function denied() {
-        // setUserError("Location is off or blocked. Enable it in your browser settings.")
+      // setUserError("Location is off or blocked. Enable it in your browser settings.")
     }
-  }, [])
+  }, []);
 
   const onSearch = (e) => {
-    setUserError(null)
+    setUserError(null);
     e.preventDefault();
     if (!city) return;
     fetchByCity(city);
@@ -44,41 +44,46 @@ export default function App({navElements}) {
   };
   const userLocation = () => {
     setLoading(true);
-    setUserError(null)
+    setUserError(null);
     if (!navigator.geolocation) {
-    alert("Geolocation is not supported by your browser.");
-    return;
+      alert("Geolocation is not supported by your browser.");
+      return;
     }
-    navigator.geolocation.getCurrentPosition(success, denied);
+    navigator.geolocation.getCurrentPosition(success, denied, {
+      enableHighAccuracy: false, 
+      timeout: 3000, 
+      maximumAge: 100000,
+    });
 
     async function success(position) {
-        const lat = position.coords.latitude;
-        const lon = position.coords.longitude;
-        const res = await fetch(`${BASE}?key=${WEATHER_API_KEY}&q=${lat},${lon}`);
-        const data = await res.json();
-        setUserCity(data.location.name);
-        if (userCity) fetchByCity(userCity);
+      const lat = position.coords.latitude;
+      const lon = position.coords.longitude;
+      const res = await fetch(`${BASE}?key=${WEATHER_API_KEY}&q=${lat},${lon}`);
+      const data = await res.json();
+      setUserCity(data.location.name);
+      if (userCity) fetchByCity(userCity);
     }
     function denied() {
-        setUserError("Location is off or blocked. Enable it in your browser settings.")
+      setUserError("Location is off or blocked. Enable it in your browser settings.");
     }
-  }
+  };
 
   return (
-    <div style={{ padding: 20 }} className="search">
-      <h1 className="title" style={{marginBottom: '20px'}}>Weather App</h1>
+    <div style={{padding: 20}} className="search">
+      <h1 className="title" style={{marginBottom: "20px"}}>
+        Weather App
+      </h1>
       <form onSubmit={onSearch}>
         <div className="form">
-          <input
-            value={city}
-            onChange={(e) => setCity(e.target.value)}
-            placeholder="Enter city (e.g. Lagos)"
-            aria-label="city"
-          />
-          <button className="button1" type="submit">Search</button>
+          <input value={city} onChange={(e) => setCity(e.target.value)} placeholder="Enter city (e.g. Lagos)" aria-label="city" />
+          <button className="button1" type="submit">
+            Search
+          </button>
         </div>
       </form>
-      <button onClick={userLocation} className="button1 button2" type="submit">Use My Current Location</button>
+      <button onClick={userLocation} className="button1 button2" type="submit">
+        Use My Current Location
+      </button>
 
       {loading && <Spinner />}
       {error && <ErrorMessage message={error} />}
